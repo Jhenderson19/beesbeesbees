4c8752ae0e5310472158a3a6d8372c053b6b5859
phucciTr <miabam26@gmail.com>
2020-11-02 20:04:43 -0800

    Complete Bare Minimum Requirements

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,13 @@
 var RetiredForagerBee = function() {
+  this = Object.create(ForagerBee);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+  this.forage = function() { return 'I am too old, let me play cards instead'; }
+  this.gamble = function() {
+    let treasure = 'treasure';
+    this.treasureChest.push(treasure);
+  }
 };
 
