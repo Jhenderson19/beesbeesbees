d1720da7bb0df41fa165f1f1eae770bf7ac8cfe1
phucciTr <miabam26@gmail.com>
2020-11-02 19:53:59 -0800

    Complete ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,11 @@
 var ForagerBee = function() {
+  this = Object.create(Bee);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.canFly = true;
+  this.treasureChest = [];
+  this.forage = function(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
 
