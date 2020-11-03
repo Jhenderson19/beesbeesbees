58c905ee77cbe403a9782f4def4f23f44b273094
phucciTr <miabam26@gmail.com>
2020-11-02 19:49:52 -0800

    Complete HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,13 @@
 var HoneyMakerBee = function() {
+  this = Object.create(Bee);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+  this.makeHoney = function() {
+    this.honeyPot++;
+  }
+  this.giveHoney = function() {
+    this.honeyPot--;
+  }
 };
 
